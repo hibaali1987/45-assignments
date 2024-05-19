186d44b1675b3e5d2f7bc94e321a0281fdadc01d
hibaali1987 <hibamaqsood_19@hotmail.com>
2024-05-20 00:33:09 +0500

    question-6 commit

---
+++ question-6/main.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const personName = `   \n\tshahid Afridi\t\n  `;
+console.log(personName);
+const without_Whitespace = personName.trim();
+console.log(without_Whitespace);
---
+++ question-6/main.ts
@@ -0,0 +1,10 @@
+
+
+const personName :string = `   \n\tshahid Afridi\t\n  `;
+
+console.log(personName);
+
+const without_Whitespace :string = personName.trim();
+
+console.log(without_Whitespace);
+
