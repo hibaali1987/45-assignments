e896e9baaa0a7a8e367b966e4ec5eae8e73a8ca4
hibaali1987 <hibamaqsood_19@hotmail.com>
2024-05-20 20:20:45 +0500

    question-7 commit

---
+++ question-7/main.js
@@ -0,0 +1,10 @@
+"use strict";
+// addition 
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log(6 + 2); // 8
+//subtraction 
+console.log(12 - 4); // 8
+//multiplication
+console.log(4 * 2); // 8
+//division
+console.log(16 / 2); //8
---
+++ question-7/main.ts
@@ -0,0 +1,12 @@
+// addition 
+
+console.log(6 + 2); // 8
+
+//subtraction 
+console.log(12-4); // 8
+
+//multiplication
+console.log(4*2); // 8
+
+//division
+console.log(16/2); //8
