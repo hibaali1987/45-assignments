a564dffe77faecc305a1ee8dc1fd2f0d4331fb3f
hibaali1987 <hibamaqsood_19@hotmail.com>
2024-05-20 21:30:22 +0500

    question-9 commit

---
+++ question-9/main.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const favourite_number = "9";
+const favourite_number_2nd = "4";
+const message = `This is my favourite number ${favourite_number}`;
+console.log(message);
---
+++ question-9/main.ts
@@ -0,0 +1,14 @@
+
+
+const favourite_number: string = "9"
+
+const favourite_number_2nd: string = "4"
+
+const message: string = `This is my favourite number ${favourite_number}`
+
+
+
+
+
+console.log(message);
+
