39363692985b7eeaf2d52ab556ea9c3fa0c15387
hibaali1987 <hibamaqsood_19@hotmail.com>
2024-05-21 21:02:43 +0500

    question-11 commit

---
+++ question-11/main.js
@@ -0,0 +1,9 @@
+"use strict";
+// Names : store the name of a few of your friends in a array called names.
+Object.defineProperty(exports, "__esModule", { value: true });
+// Print each person's name by accessing each element in the list ,once at a time.
+let friends_names = ['Hiba', 'Ali',];
+console.log(friends_names);
+for (let i = 0; i > friends_names.length; i++) {
+    console.log(friends_names[i]);
+}
---
+++ question-11/main.ts
@@ -0,0 +1,17 @@
+// Names : store the name of a few of your friends in a array called names.
+
+// Print each person's name by accessing each element in the list ,once at a time.
+
+ let friends_names: string[] = ['Hiba' ,'Ali' ,];
+
+console.log(friends_names);
+
+for (let i=0; i>friends_names.length; i++){ 
+
+    console.log(friends_names[i]);
+
+
+}
+ 
+
+
