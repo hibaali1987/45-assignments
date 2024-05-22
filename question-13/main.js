303540b874dd63afa25eebabca074b6a1e7bf3fb
hibaali1987 <hibamaqsood_19@hotmail.com>
2024-05-22 13:34:16 +0500

    question-13 commit

---
+++ question-13/main.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let transportation = ['car', 'bike', 'bus', 'private jet', 'train'];
+for (let index = 0; index < transportation.length; index++) {
+    console.log('i would like to own a' + transportation[index]);
+}
---
+++ question-13/main.ts
@@ -0,0 +1,8 @@
+
+let transportation: string[] = ['car', 'bike' ,'bus', 'private jet' ,'train']
+
+for (let index = 0; index < transportation.length; index++) {
+
+    console.log('i would like to own a' +transportation[index]);
+    
+}
