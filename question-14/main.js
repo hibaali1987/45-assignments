b99f24decad1b1a518ea4d129d158deaf668a215
hibaali1987 <hibamaqsood_19@hotmail.com>
2024-05-22 13:55:57 +0500

    question-14 commit

---
+++ question-14/main.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let guestList = ['Hiba', 'ali', 'arham', 'ahmad'];
+let message = "you are invited in Dinner";
+for (let index = 0; index < guestList.length; index++) {
+    console.log('Mr.' + guestList[index] + '  ' + message);
+}
---
+++ question-14/main.ts
@@ -0,0 +1,11 @@
+
+let guestList: string[] = ['Hiba', 'ali' ,'arham' ,'ahmad']
+
+let message: string = "you are invited in Dinner"
+
+for (let index = 0; index < guestList.length; index++) {
+
+    console.log('Mr.' +  guestList[index] +  '  '  +  message );
+
+
+}
