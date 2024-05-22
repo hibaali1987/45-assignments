7d6ff2e216a99e4533b5a9085e6e160b10a4a365
hibaali1987 <hibamaqsood_19@hotmail.com>
2024-05-22 13:17:07 +0500

    question-12 commit

---
+++ question -12/main.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let friends_names = ['Hiba', 'Ali', 'Arham', 'Ahmad',];
+let message = "nice to meet u!";
+for (let i = 0; i < friends_names.length; i++) {
+    console.log(message + friends_names[i]);
+}
---
+++ question -12/main.ts
@@ -0,0 +1,13 @@
+
+
+let friends_names: string[] = ['Hiba' ,'Ali', 'Arham' , 'Ahmad',];
+
+let message: string = "nice to meet u!"
+
+for (let i=0; i<friends_names.length; i++){ 
+
+    console.log(message + friends_names[i]);
+
+
+}
+ 
