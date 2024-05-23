98d872c0cd7935b15e332591eb72fa368e244635
hibaali1987 <hibamaqsood_19@hotmail.com>
2024-05-23 22:39:09 +0500

    question-26 commit

---
+++ question-26/main.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let aliencolor = "green";
+// version1
+if (aliencolor === "green") {
+    console.log("the player just earned 5 points.");
+}
+else {
+    console.log("the player just earned 10 points");
+}
+//version2
+if (aliencolor === "yellow") {
+    console.log("the player just earned 5 points.");
+}
+else {
+    console.log("the player just earned 10 points");
+}
---
+++ question-26/main.ts
@@ -0,0 +1,24 @@
+
+
+let aliencolor = "green"
+
+// version1
+
+if(aliencolor === "green") {
+    console.log("the player just earned 5 points.");
+
+}else{
+    console.log("the player just earned 10 points");
+}
+
+
+//version2
+
+
+
+if(aliencolor === "yellow") {
+    console.log("the player just earned 5 points.");
+
+}else{
+    console.log("the player just earned 10 points");
+}
