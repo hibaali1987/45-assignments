dd27ca47c094e63126058a866f6e5fe50b63dea9
hibaali1987 <hibamaqsood_19@hotmail.com>
2024-05-23 22:52:24 +0500

    question-27 commit

---
+++ question-27/main.ts
@@ -0,0 +1,40 @@
+
+
+let alien_color1 = "green"
+
+//version 1
+if(alien_color1 === "green") {
+    console.log("the player just earned 5 points.");
+
+}else{
+    console.log("the player just earned 10 points");
+
+}
+
+let alien_color2 = "yellow"
+// version2
+
+if(alien_color2 === "yellow") {
+    console.log("the player just earned 15 points.");
+
+}
+else{
+    console.log("the player just earned 5 points");
+
+
+}
+
+
+let alien_color3 = "red"
+// version3
+
+if(alien_color3 === "red") {
+    console.log("the player just earned 10 points.");
+
+}
+else{
+    console.log("the player just earned 5 points");
+
+
+}
+
