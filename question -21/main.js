05ef8460702748ee6af5d02a55fc9644a3aad8de
hibaali1987 <hibamaqsood_19@hotmail.com>
2024-05-23 19:42:12 +0500

    question-21 commit

---
+++ question -21/main.js
@@ -0,0 +1,7 @@
+"use strict";
+let person = {
+    name: "Hiba",
+    age: " 36",
+    isStudent: true
+};
+console.log(person.name);
---
+++ question -21/main.ts
@@ -0,0 +1,15 @@
+
+
+type Tperson ={
+    name:string
+    age:number
+    isStudent: boolean
+}
+let person = {
+
+    name: "Hiba",
+    age: " 36",
+    isStudent: true
+}
+
+console.log(person.name);
