4ddc58b0ebf8ba0aaa89146191da15935d8e1021
hibaali1987 <hibamaqsood_19@hotmail.com>
2024-05-25 02:02:47 +0500

    question-41 commit

---
+++ question-41/main.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let magician = ['harry potter', 'hermoine granger', 'ron waesley', 'albus dumbledore'];
+function show_magicians(magicians) {
+    magician.forEach(element => {
+        console.log(element);
+    });
+}
+show_magicians(magician);
---
+++ question-41/main.ts
@@ -0,0 +1,14 @@
+
+
+let magician :string[] =['harry potter','hermoine granger','ron waesley','albus dumbledore'];
+
+function show_magicians(magicians: string[]){
+
+    magician.forEach(element => {
+        console.log(element);
+
+    });
+
+}
+
+show_magicians(magician)
